8e67f3953cb1b3180c9a30d12629b6f344ef993f
kharioki <antolisah@gmail.com>
2020-07-27 22:45:59 +0300

    comparing class and functional component context consumers

--- use_context/src/ClassContextComponent.js
+++ use_context/src/ClassContextComponent.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ThemeContext } from './App';
 
-export default function ClassContextComponent() {
-  return <></>;
+export default class ClassContextComponent extends Component {
+  themeStyles(dark) {
+    return {
+      backgroundColor: dark ? '#333' : '#ccc',
+      color: dark ? '#ccc' : '#333',
+      padding: '2rem',
+      margin: '2rem'
+    };
+  }
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {darkTheme => {
+          return <div style={this.themeStyles(darkTheme)}>Class Theme</div>;
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
 }
--- use_context/src/FunctionContextComponent.js
+++ use_context/src/FunctionContextComponent.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from './App';
 
 export default function FunctionContextComponent() {
-  return <></>;
+  const darkTheme = useContext(ThemeContext);
+  const themeStyles = {
+    backgroundColor: darkTheme ? '#333' : '#ccc',
+    color: darkTheme ? '#ccc' : '#333',
+    padding: '2rem',
+    margin: '2rem'
+  };
+
+  return <div style={themeStyles}>Function Theme</div>;
 }
